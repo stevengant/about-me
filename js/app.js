f0391d131c303d03e097e02e334e3c1afac9b942
stevengant <sgant39@gmail.com>
2022-11-30 17:22:53 +0000

    fixed toLocaleLowerCase typo

--- js/app.js
+++ js/app.js
@@ -4,35 +4,35 @@ let userName = prompt('Hi! What\'s your name?');
 
 alert(`Welcome to my site ${userName}! Please answer the following questions yes/no (or y/n)`);
 
-let questionOneGuess = prompt('Do I have two kids?').toLocaleLowerCase();
+let questionOneGuess = prompt('Do I have two kids?').toLowerCase();
 
 if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
   alert("Yeah, you got it!");
 } else if(questionOneGuess === 'no' || questionOneGuess === 'n') {
   alert("Nope. Wrong answer.")
 } 
-let questionTwoGuess = prompt('Do I have four dogs?').toLocaleLowerCase();
+let questionTwoGuess = prompt('Do I have four dogs?').toLowerCase();
 
 if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
   alert("Yeah, you got it!");
 } else if(questionTwoGuess === 'no' || questionTwoGuess === 'n') {
   alert("Nope. Wrong answer.")
 } 
-let questionThreeGuess = prompt('Do I like pickled beets?').toLocaleLowerCase();
+let questionThreeGuess = prompt('Do I like pickled beets?').toLowerCase();
 
 if (questionThreeGuess === 'no' || questionThreeGuess === 'n') {
   alert("Yeah, you got it!");
 } else if(questionThreeGuess === 'yes' || questionThreeGuess === 'y') {
   alert("Nope. Wrong answer.")
 } 
-let questionFourGuess = prompt('Do I enjoy snowboarding?').toLocaleLowerCase();
+let questionFourGuess = prompt('Do I enjoy snowboarding?').toLowerCase();
 
 if (questionFourGuess === 'yes' || questionFourGuess === 'y') {
   alert("Yeah, you got it!");
 } else if(questionFourGuess === 'no' || questionFourGuess === 'n') {
   alert("Nope. Wrong answer.")
 } 
-let questionFiveGuess = prompt('Is coding fun?').toLocaleLowerCase();
+let questionFiveGuess = prompt('Is coding fun?').toLowerCase();
 
 if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
   alert("Heck yeah it is! High fives!!");
