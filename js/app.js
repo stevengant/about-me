fdcfe1392abd92ff837e61beaacf7f3dea8474f6
stevengant <sgant39@gmail.com>
2022-12-01 20:18:05 +0000

    tweaked code during lecture

--- js/app.js
+++ js/app.js
@@ -42,20 +42,36 @@ if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
 
 
 // QUESTION SIX
-// assign answer to variable myNum and initialize guessNum to zero
+// As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
+
+
+// DONE: Set a number for user to guess.
 let myNum = 8;
+
+
+
+// After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+// collabed with Liam Grist and Brendan Moore
+
+
+// DONE: It should give the user exactly four opportunities to get the correct answer.
 let guessNum = 4;
 
+// assign answer to variable myNum and initialize guessNum to zero
 while(guessNum > 0) {
 
-  let questionSixGuess = prompt('I\'m thinking of a number between 1 and 10. Can you guess it in 4 tries?');
+// DONE: Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+  let questionSixGuess = +prompt('I\'m thinking of a number between 1 and 10. Can you guess it in 4 tries?');
 
-  if (parseInt(questionSixGuess) === myNum) {
+// DONE: Indicate user got it right.
+  if (questionSixGuess === myNum) {
     alert(`That's great ${userName}! You got it!`);
     break;
 
+// DONE: Indicate if “too low”.
   } else if (questionSixGuess < myNum) {
     alert(`You're guess is too low ${userName}. Guess again.`);
+// DONE: Indicates through an alert if the guess is “too high” 
 
   } else if (questionSixGuess > myNum) {
     alert(`Your guess is too high ${userName}. Guess again.`);
@@ -67,17 +83,27 @@ while(guessNum > 0) {
 alert(`The correct answer was ${myNum}`);
 
 
-// QUESTION SIX
+// QUESTION SEVEN
+
+
+
+// TODO: they run out of attempts.
+
+// Consider using a loop of some sort for this question.
 // collabed with Liam Grist and Brendan Moore
 let dreamVaca = ['egypt', 'ireland', 'sweden', 'beliz', 'switzerland', 'alaska'];
 
+// DONE: Give the user 6 attempts to guess the correct answer
 for(let i = 0; i < 6; i++) {
+
+  // DONE: Add a 7th question that has multiple possible correct answers that are stored in an array.
   let questionSevenGuess = prompt('Now can you guess where I want to vacation?').toLowerCase();
   let correct = false;
   for (let j = 0; j < dreamVaca.length; j++) {
-    console.log(dreamVaca[j]);
-    console.log(questionSevenGuess);
+
     if(questionSevenGuess === dreamVaca[j]) {
+      alert('Yes! That\'s one of my places!')
+      score++;
       correct = true;
       break;
     }
@@ -86,9 +112,11 @@ for(let i = 0; i < 6; i++) {
   }
   if (correct === false) {
     alert('Sorry that\'s not one of my places.'); 
+    // DONE: The guesses will end once the user guesses a correct answer 
   } else if(correct === true) {
     alert(`Yeah, you guessed one of my dream vacations ${userName}!`);
     break;
   }
 }
-alert(`Thanks for playing ${userName}!`);
+// DONE: Display all the possible correct answers to the user.
+alert(`Thanks for playing ${userName}! My dream vacations are ${dreamVaca}.`);
